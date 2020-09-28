ed70677e6fee1894d48189040a612753d1e8220c
Sehrish88 <sehrishbalouch4@gmail.com>
2020-09-28 15:29:57 -0400

    create and add  session_store and cors.rb files

---
+++ skill_swap_api/config/initializers/session_store.rb
@@ -0,0 +1,5 @@
+Rails.application.config.session_store :cookie_store, :key => '_skill_swap_api'
+# if Rails.env.production == 'production'
+#   Rails.application.config.session_store :cookie_store, :key => '__skill_swap', domain: 
+# else
+# end
