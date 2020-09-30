4aab562bc8c8e629f0da8736e5312ad1caa18cef
Sehrish88 <sehrishbalouch4@gmail.com>
2020-09-30 15:38:41 -0400

    create react app component and have it return dashboard and home componenets

---
+++ skill_swap_client/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import Home from './components/Home';
+import Dashboard from './components/Dashboard'; 
+
+function App() {
+    return (
+      <div className="App">
+        <Router>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/dashboard" component={Dashboard} />
+          </Switch>
+        </Router>
+      </div>
+    );
+  }
+  
+  export default App; 
