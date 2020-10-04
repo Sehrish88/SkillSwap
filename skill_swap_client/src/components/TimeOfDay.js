4239e38614ceb51ca09ecd1d26374920f86833a9
Sehrish88 <sehrishbalouch4@gmail.com>
2020-10-04 19:59:55 -0400

    add TimeOfDay component to greet users based on time of day

---
+++ skill_swap_client/src/components/TimeOfDay.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+function TimeOfDay() {
+  
+        const date = new Date()
+        const hours = date.getHours()
+        let timeOfDay
+        
+        if (hours < 12) {
+          timeOfDay = "morning"
+        } else if (hours >= 12 && hours < 17) {
+          timeOfDay = "afternoon"
+        } else {
+          timeOfDay = "night"
+        }
+        
+        return (
+          <h3 style={{color: "green"}}>We hope you're having a great {timeOfDay}!</h3>
+        ) 
+      
+
+}
+
+export default TimeOfDay 
