98c4bb258f40a058359361aa30973b631c631896
Sehrish88 <sehrishbalouch4@gmail.com>
2020-10-04 20:06:29 -0400

    add welcome message and icon to home page

--- skill_swap_client/src/components/Home.js
+++ skill_swap_client/src/components/Home.js
@@ -1,8 +1,16 @@
 import React from 'react';
+import {MdSwapCalls} from 'react-icons/md';
+
 
 const Home = () => {
-    return <h3> Home </h3>; 
+    return <h1 style={{color: 'green', textAlign: 'center'}}>Welcome To Skill Swap
+        <MdSwapCalls />
+    </h1>
+     
 
 }
 
 export default Home; 
+
+
+//
