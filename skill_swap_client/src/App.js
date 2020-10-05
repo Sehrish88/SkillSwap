9293a50904bd82d7e4a5d46c6400cfbd3489c133
Sehrish88 <sehrishbalouch4@gmail.com>
2020-10-05 10:30:16 -0400

    delete spacing between routes

--- skill_swap_client/src/App.js
+++ skill_swap_client/src/App.js
@@ -12,7 +12,6 @@ function App() {
         <Router>
           <Switch>
             <Route exact path="/" component={Home} />
-
             <Route exact path="/dashboard" component={Dashboard} />
           </Switch>
         </Router>
