e16a98ee6d9e7878dfa47cde383da9174d42bfe2
Sehrish88 <sehrishbalouch4@gmail.com>
2020-10-04 20:05:32 -0400

    import TimeOfDay component to App.js file

--- skill_swap_client/src/App.js
+++ skill_swap_client/src/App.js
@@ -2,16 +2,24 @@ import React from 'react';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import Home from './components/Home';
 import Dashboard from './components/Dashboard'; 
+import TimeOfDay from './components/TimeOfDay';
+//import {MdSwapCalls} from 'react-icons/md';
 
 function App() {
     return (
       <div className="App">
+         
         <Router>
           <Switch>
             <Route exact path="/" component={Home} />
+
             <Route exact path="/dashboard" component={Dashboard} />
           </Switch>
         </Router>
+        <TimeOfDay />
+        <div>
+         
+        </div>
       </div>
     );
   }
