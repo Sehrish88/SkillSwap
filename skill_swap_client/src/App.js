5fd25a330c8befdbc433582d634a2c2dd2449214
Sehrish88 <sehrishbalouch4@gmail.com>
2020-10-06 13:27:18 -0400

    remove import statements

--- skill_swap_client/src/App.js
+++ skill_swap_client/src/App.js
@@ -3,24 +3,31 @@ import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import Home from './components/Home';
 import Dashboard from './components/Dashboard'; 
 import TimeOfDay from './components/TimeOfDay';
+import UsersContainer from './components/UsersContainer' 
+import { connect } from 'react-redux'
+import { fetchUsers } from './actions/usersActions'
 //import {MdSwapCalls} from 'react-icons/md';
 
-function App() {
+function App(props) {
     return (
       <div className="App">
+       
+    
          
         <Router>
           <Switch>
             <Route exact path="/" component={Home} />
             <Route exact path="/dashboard" component={Dashboard} />
+            <Route exact path="/users" component={UsersContainer} />
           </Switch>
-        </Router>
+        </Router> 
         <TimeOfDay />
-        <div>
+         </div>
+       
+
          
-        </div>
-      </div>
+    
     );
   }
   
-  export default App; 
+  export default connect(null, { fetchUsers})(App); 
