637941406629a1fbf4da426fb3c30aaa8eeff805
Sehrish88 <sehrishbalouch4@gmail.com>
2020-10-06 13:17:34 -0400

    add fetch action to usersAction file

---
+++ skill_swap_client/src/actions/usersActions.js
@@ -0,0 +1,26 @@
+
+
+export const  fetchUsers= () => {
+    return (dispatch) => {
+        fetch('http://127.0.0.1:3000/users')
+        .then(resp => resp.json())
+        .then(users => dispatch({ type: 'FETCH_USERS', payload: users})) 
+    }
+       
+} 
+
+export const addUser = user => {
+    console.log(user)
+    return dispatch => {
+        fetch('http://127.0.0.1:3000/users', {
+        method: 'POST', 
+        body: JSON.stringify({user}),
+        headers: { 'Content-Type': 'application/json'}
+    })
+    .then(resp => resp.json())
+    .then(user => dispatch({ type: 'ADD_USER', payload: user})) 
+
+    }
+    
+
+} 
