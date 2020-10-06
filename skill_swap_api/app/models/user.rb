1a7de4c04b35140f154d01c4170088c53afb7188
Sehrish88 <sehrishbalouch4@gmail.com>
2020-10-06 13:26:14 -0400

    write sttribute writer method to have users accept nested attributes for skills

--- skill_swap_api/app/models/user.rb
+++ skill_swap_api/app/models/user.rb
@@ -1,4 +1,11 @@
 class User < ApplicationRecord
   has_secure_password
   has_many :skills
-end
+  #accepts_nested_attributes_for :skills
+
+  def skills_attributes=(hash)
+    # custom attribute writer
+    self.skills.build(field:  hash[:field], years_experience: hash[:years_experience])
+    self.save 
+  end
+end 
