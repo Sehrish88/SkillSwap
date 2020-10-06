da2b7590b95ce771c6e43e668c05a0e00fb35ef6
Sehrish88 <sehrishbalouch4@gmail.com>
2020-10-06 13:19:02 -0400

    create store and apply middleware, thunk and provider fromm redux

--- skill_swap_client/src/index.js
+++ skill_swap_client/src/index.js
@@ -1,8 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App'
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux'
+import thunk from 'redux-thunk' 
+import { composeWithDevTools } from 'redux-devtools-extension';
+import rootReducer from './reducers/rootReducer' 
+
+
+const store = createStore(
+    rootReducer,
+    composeWithDevTools(applyMiddleware(thunk))
+) 
+
 
 ReactDOM.render(
-    <App />,
-    document.querySelector('#root')
+<Provider store={store}>
+<App />
+</Provider>, 
+   document.getElementById('root')
 );
