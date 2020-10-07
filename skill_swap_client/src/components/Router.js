cedba68128f3b04a1a1df9b1750c2d5cf3a84422
Sehrish88 <sehrishbalouch4@gmail.com>
2020-10-07 10:57:35 -0400

    create a router component file

---
+++ skill_swap_client/src/components/Router.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Switch, Route } from 'react-router-dom'
+
+
+const Router = () => {
+    return (
+        <div>
+
+        </div>
+    )
+}; 
+
+export default Router; 
