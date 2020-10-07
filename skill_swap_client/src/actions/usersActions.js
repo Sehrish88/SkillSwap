48ee3baa132ef6baa6b33c9d8990c1575088b396
Sehrish88 <sehrishbalouch4@gmail.com>
2020-10-07 18:41:52 -0400

    add log in action toe registration form

--- skill_swap_client/src/actions/usersActions.js
+++ skill_swap_client/src/actions/usersActions.js
@@ -22,5 +22,17 @@ export const addUser = user => {
 
     }
     
+    const userLogin = (state = {currentUser: {}, loggedIn: false}, action) => {
+
+        switch(action.type) {
+            case 'LOGIN':
+                return {...state, loggedIn: true, currentUser : action.payload}
+
+            case 'LOGOUT':
+                return {...state, loggedIn: false, currentUser: {}}
+
+        }
+    }
 
 } 
+
