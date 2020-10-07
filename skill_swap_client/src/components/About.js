a97311725f9f662f566d7c8f59b6ceadf0f35614
Sehrish88 <sehrishbalouch4@gmail.com>
2020-10-07 11:03:46 -0400

    add text, border, and styling to about page

--- skill_swap_client/src/components/About.js
+++ skill_swap_client/src/components/About.js
@@ -2,7 +2,8 @@ import React from 'react'
 
 
 const About = () => {
-    return <h3> About  </h3>; 
+    return (
+    <h4 style={{color: 'green', textAlign: 'center', border: '5px solid green'}}> Skill swap is a platform created for the exchange of knowledge, ideas, and skills. It was designed to connect individuals with others who will help them grow and in turn, reduce the skills gap that we see in society today.   We aim for inclusivity and to make it easier for people to gain access to resources to which money may be a barrier in certain instances. Skill Swap’s main goal is to help people feel empowered, connected, and live life with a sense of purpose while helping societies become more functional.  </h4> )
 
 }
 
