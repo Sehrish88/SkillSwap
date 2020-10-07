b1b321922677128af85a18532398b23deb6b6c21
Sehrish88 <sehrishbalouch4@gmail.com>
2020-10-07 10:59:07 -0400

    import BrowserRouter from react-redux

--- skill_swap_client/src/index.js
+++ skill_swap_client/src/index.js
@@ -6,7 +6,7 @@ import { Provider } from 'react-redux'
 import thunk from 'redux-thunk' 
 import { composeWithDevTools } from 'redux-devtools-extension';
 import rootReducer from './reducers/rootReducer' 
-
+import { BrowserRouter as Router } from 'react-router-dom'; 
 
 const store = createStore(
     rootReducer,
@@ -16,7 +16,10 @@ const store = createStore(
 
 ReactDOM.render(
 <Provider store={store}>
-<App />
+ <Router>
+    <App /> 
+ </Router>   
+
 </Provider>, 
    document.getElementById('root')
 );
