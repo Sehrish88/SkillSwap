505bfb9c2b494a027815575ed3756db79bc4bccb
Sehrish88 <sehrishbalouch4@gmail.com>
2020-10-07 18:44:01 -0400

    add resources for sessions

--- skill_swap_api/config/routes.rb
+++ skill_swap_api/config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  resources :sessions 
   resources :skills
+  resources :registrations 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'pages#home'
   resources :sessions, only: [:create]
