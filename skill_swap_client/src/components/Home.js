6f628b7976a0e9203eca4baea3565cc07e8846db
Sehrish88 <sehrishbalouch4@gmail.com>
2020-10-09 10:39:24 -0400

    import NavBar to Home file

--- skill_swap_client/src/components/Home.js
+++ skill_swap_client/src/components/Home.js
@@ -1,12 +1,29 @@
 import React from 'react';
 import {MdSwapCalls} from 'react-icons/md';
+import TimeOfDay from './TimeOfDay'
+import NavBar from './NavBar'
+
+
 
 
 const Home = () => {
-    return <h1 style={{color: 'green', textAlign: 'center', border: '5px solid green'}}> Skill Swap
-        <MdSwapCalls />
+    return (
+        <div>
+       
+
+
+        <h1 style={{color: 'green', textAlign: 'center', border: '5px solid green'}}> Skill Swap <MdSwapCalls />  </h1>
+        <NavBar />
+
+
         
-    </h1>
+        
+     </div>
+
+    )
+ 
+    
+ 
      
 
 }
