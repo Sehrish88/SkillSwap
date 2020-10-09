66aa7c23de3dd0f634c944fd640627ba39fefd81
Sehrish88 <sehrishbalouch4@gmail.com>
2020-10-09 10:47:56 -0400

    update user controllers to render json responses

--- skill_swap_api/app/controllers/users_controller.rb
+++ skill_swap_api/app/controllers/users_controller.rb
@@ -37,7 +37,7 @@ class UsersController < ApplicationController
       if @user.save
         session[:id] = @user.id 
         format.html { redirect_to @user, notice: 'User was successfully created.' }
-        format.json { render :show, status: :created, location: @user }
+        format.json { render json:  {user: @user, logged_in: true}}
       else
         format.html { render :new }
         format.json { render json: @user.errors, status: :unprocessable_entity }
