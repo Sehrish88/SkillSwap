45777c2c29330aeb430240402cf633553f74d9c6
Sehrish88 <sehrishbalouch4@gmail.com>
2020-10-09 10:31:36 -0400

    no change

--- skill_swap_client/src/reducers/usersReducer.js
+++ skill_swap_client/src/reducers/usersReducer.js
@@ -2,8 +2,9 @@ export const usersReducer = (state = [], action) => {
     switch(action.type){
         case 'FETCH_USERS':
             return action.payload
-            case 'ADD_USER':
+        case 'ADD_USER':
             return [...state, action.payload] 
+        
 
              default:
             return state 
