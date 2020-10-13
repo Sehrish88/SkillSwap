21199d0f46312770bb3a91cca6ed5284e4ab3ea6
Sehrish88 <sehrishbalouch4@gmail.com>
2020-10-13 12:00:35 -0400

    add componenet did mount method

--- skill_swap_client/src/App.js
+++ skill_swap_client/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import Home from './components/Home';
 import About from './components/About';
@@ -7,38 +7,50 @@ import TimeOfDay from './components/TimeOfDay';
 import UsersContainer from './components/UsersContainer' 
 import { connect } from 'react-redux'
 import { fetchUsers } from './actions/usersActions'
-import Login from './components/LoginForm'; 
 import RegistrationForm from './components/RegistrationForm';
 import LoginForm from './components/LoginForm';
 import NavBar from './components/NavBar'
 
+
 //import {MdSwapCalls} from 'react-icons/md';
 
-function App(props) {
+class App extends Component  {
+    
+  componentDidMount() {
+    this.props.fetchUsers()
+    }
+  render () {
     return (
       <div className="App">
-       <TimeOfDay />
-    
-         
-        <Router>
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route exact path="/dashboard" component={Dashboard} />
-            <Route exact path="/users" component={UsersContainer} />
-            <Route exact path="/about" component={About} />
-            <Route exact path="/login" component={LoginForm} />
-            <Route exact path="/register" component={RegistrationForm} /> 
+      <TimeOfDay />
+   
+        
+       <Router>
+         <Switch>
+           <Route exact path="/" component={Home} />
+           <Route  path="/dashboard" component={Dashboard} />
+           <Route  path="/users" component={UsersContainer} />
+           <Route  path="/about" component={About} />
+           <Route  path="/login" component={LoginForm} />
+           <Route  path="/register" component={RegistrationForm} /> 
 
-          </Switch>
-         
-        </Router> 
+         </Switch>
+        
+       </Router> 
+    
+        </div>
+      
+
+    ) 
+
      
-         </div>
-       
 
          
     
-    );
+    
+
+  }
+    
   }
   
   export default connect(null, { fetchUsers})(App); 
