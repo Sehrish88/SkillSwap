f4b6b5564b351e3119779b4555cbabce6833af67
Sehrish88 <sehrishbalouch4@gmail.com>
2020-10-13 11:49:56 -0400

    create header

---
+++ skill_swap_client/src/components/Header.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import {MdSwapCalls} from 'react-icons/md';
+
+const Header = () => {
+
+    return <h1 style={{color: 'green', textAlign: 'center', border: '5px solid green'}}> Skill Swap <MdSwapCalls />  </h1>
+}
+
+export default Header; 
