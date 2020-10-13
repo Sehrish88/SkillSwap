f84a5d3f5953599f2502f76434aa24a7f982ea8a
Sehrish88 <sehrishbalouch4@gmail.com>
2020-10-13 11:51:18 -0400

    remove lifecycle method

--- skill_swap_client/src/components/UsersContainer.js
+++ skill_swap_client/src/components/UsersContainer.js
@@ -4,19 +4,23 @@ import { fetchUsers } from '../actions/usersActions';
 import RegistrationForm from './RegistrationForm'; 
 import UsersList from './UsersList' 
 import {MdSwapCalls} from 'react-icons/md';
+import Dashboard from './Dashboard'
 
 
  class UsersContainer extends Component {
-     componentDidMount() {
-         this.props.fetchUsers()
-     }
+     
 
     render() {
         return (
             <div>
                   <h1 style={{color: 'green', textAlign: 'center'}}> Skill Swap <MdSwapCalls /> </h1>
 
+               
+
                 <UsersList />
+                
+                <Dashboard /> 
+                
                
             </div>
         )
