9312bec21e3373c8337fbc5e52278efb6cf431fb
Sehrish88 <sehrishbalouch4@gmail.com>
2020-10-17 16:13:46 -0400

    remove uneccasry dashboard link from navbar

--- skill_swap_client/src/components/NavBar.js
+++ skill_swap_client/src/components/NavBar.js
@@ -7,10 +7,11 @@ const NavBar = () => {
             <h6 style={{color: 'green', }}><ul >
                <NavLink to="/">Home</NavLink> <br />
                <NavLink to="/register">Register</NavLink><br />
+               <NavLink to="/addSkill">Add Skill</NavLink><br />
                <NavLink to="/about">About</NavLink><br />
                <NavLink to="/users">Browse Users</NavLink><br />
-               <NavLink to="/login">Log In</NavLink><br />
-               <NavLink to="/dashboard">Dashboard</NavLink><br /> 
+               <NavLink to="/skills">Browse skills</NavLink><br />
+              
               
             </ul></h6>
         </div>
