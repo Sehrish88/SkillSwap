24debac0c6914cd5f015c9c85056437740963f1b
Sehrish88 <sehrishbalouch4@gmail.com>
2020-10-22 16:29:14 -0400

    import and display home link to users list

--- skill_swap_client/src/components/UsersList.js
+++ skill_swap_client/src/components/UsersList.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import HomeLink from './HomeLink'
+
 
 const UsersList = ({users}) => {
     console.log(users)
     return(
         <div>
+           <HomeLink />
             {users.map(user => <ul><li key={user.id} style= {{border: '3px solid green'}}><h4>{user.name}</h4>  <b>Email:</b>{user.email} <b>Field</b> {user.skills[0].field} <b> Years of Experience-</b> {user.skills[0].years_experience} </li></ul>)}
+            
+            
         </div>
     )
 }
