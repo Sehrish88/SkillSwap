c304dd26a8920ef96aa37dac392c40ca1f11134f
Sehrish88 <sehrishbalouch4@gmail.com>
2020-10-22 16:19:03 -0400

    change position of about page link in the navbar

--- skill_swap_client/src/components/NavBar.js
+++ skill_swap_client/src/components/NavBar.js
@@ -6,9 +6,9 @@ const NavBar = () => {
         <div className="Container">
             <h6 style={{color: 'green', }}><ul >
                <NavLink to="/">Home</NavLink> <br />
+               <NavLink to="/about">About</NavLink><br />
                <NavLink to="/register">Register</NavLink><br />
                <NavLink to="/addSkill">Add Skill</NavLink><br />
-               <NavLink to="/about">About</NavLink><br />
                <NavLink to="/users">Browse Users</NavLink><br />
                <NavLink to="/skills">Browse skills</NavLink><br />
               
