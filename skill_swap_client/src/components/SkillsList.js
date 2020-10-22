00ae809c584f11833dc2e23dd9c6135449493ccc
Sehrish88 <sehrishbalouch4@gmail.com>
2020-10-22 16:26:35 -0400

    add home link to skills list page

--- skill_swap_client/src/components/SkillsList.js
+++ skill_swap_client/src/components/SkillsList.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 import { fetchSkills } from '../actions/skillsActions'
 import Header from './Header'
 import Skill from './Skill'
+import HomeLink from './HomeLink'
 
 class  SkillsList extends Component {
  
@@ -15,6 +16,7 @@ class  SkillsList extends Component {
         return(
             <div>
                  <Header /> 
+                 <HomeLink />
                 {this.props.skills.map(skill => <Skill skill={skill} />)}
             </div>
         )
