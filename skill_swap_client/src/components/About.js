96de998f9424921401c039349c9a441c3b39a7b7
Sehrish88 <sehrishbalouch4@gmail.com>
2020-10-22 16:18:14 -0400

    add home link component to about page

--- skill_swap_client/src/components/About.js
+++ skill_swap_client/src/components/About.js
@@ -1,11 +1,16 @@
 import React from 'react'
+import HomeLink from './HomeLink'
 
 
 const About = () => {
     return (
-        
-    <h4 style={{color: 'green', textAlign: 'center', border: '5px solid green'}}> Skill swap is a platform created for the exchange of knowledge, ideas, and skills. It was designed to connect individuals with others who will help them grow and in turn, reduce the skills gap that we see in society today.   We aim for inclusivity and to make it easier for people to gain access to resources to which money may be a barrier in certain instances. Skill Swap’s main goal is to help people feel empowered, connected, and live life with a sense of purpose while helping societies become more functional.  </h4> )
 
+    <div> 
+    <HomeLink />
+    <h4 style={{color: 'green', textAlign: 'center', border: '5px solid green'}}> Skill swap is a platform created for the exchange of knowledge, ideas, and skills. It was designed to connect individuals with others who will help them grow and in turn, reduce the skills gap that we see in society today.   We aim for inclusivity and to make it easier for people to gain access to resources to which money may be a barrier in certain instances. Skill Swap’s main goal is to help people feel empowered, connected, and live life with a sense of purpose while helping societies become more functional.  </h4>
+    </div>
+     )
+    
 }
 
 export default About;
