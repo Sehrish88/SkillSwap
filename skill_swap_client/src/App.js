9e6b21fc506ec789a4f6c744d3aaf7fa6e85a2f4
Sehrish88 <sehrishbalouch4@gmail.com>
2020-10-23 13:58:08 -0400

    remove uneceaasary dashboard and login imports of deleted files

--- skill_swap_client/src/App.js
+++ skill_swap_client/src/App.js
@@ -2,13 +2,11 @@ import React, { Component } from 'react';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import Home from './components/Home';
 import About from './components/About';
-import Dashboard from './components/Dashboard'; 
 import TimeOfDay from './components/TimeOfDay';
 import UsersContainer from './components/UsersContainer' 
 import { connect } from 'react-redux'
 import { fetchUsers } from './actions/usersActions'
 import RegistrationForm from './components/RegistrationForm';
-import LoginForm from './components/LoginForm';
 import NavBar from './components/NavBar'
 import SkillsList from './components/SkillsList';
 import SkillsForm from './components/SkillsForm' 
@@ -28,10 +26,8 @@ class App extends Component  {
        <Router>
          <Switch>
            <Route exact path="/" component={Home} />
-           <Route  path="/dashboard" component={Dashboard} />
            <Route  path="/users" component={UsersContainer} />
            <Route  path="/about" component={About} />
-           <Route  path="/login" component={LoginForm} />
            <Route  path="/register" component={RegistrationForm} /> 
            <Route  path="/addSkill" component={SkillsForm} /> 
            <Route exact path="/skills" component={SkillsList} />
