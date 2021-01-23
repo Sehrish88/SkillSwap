0d4c5905e97bb613657afa5d5ed28ee78a9a082a
Sehrish88 <sehrishbalouch4@gmail.com>
2021-01-23 13:48:44 -0500

    add background color to header

--- skill_swap_client/src/components/Header.js
+++ skill_swap_client/src/components/Header.js
@@ -3,7 +3,7 @@ import {MdSwapCalls} from 'react-icons/md';
 
 const Header = () => {
 
-    return <h1 style={{color: 'green', textAlign: 'center', border: '5px solid green'}}> Skill Swap <MdSwapCalls />  </h1>
+    return <h1 style={{color: 'green', textAlign: 'center', border: '5px solid green', backgroundColor: 'skyblue', className: 'header'}}> Skill Swap <MdSwapCalls />  </h1>
 }
 
 export default Header; 
