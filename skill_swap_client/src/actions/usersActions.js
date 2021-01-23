be16984b5e059daa05c5b87e550d28e0dae9a9bb
Sehrish88 <sehrishbalouch4@gmail.com>
2021-01-23 13:45:41 -0500

    get rid of unnecessary console.logs

--- skill_swap_client/src/actions/usersActions.js
+++ skill_swap_client/src/actions/usersActions.js
@@ -2,29 +2,32 @@
 
 export const  fetchUsers= () => {
     return (dispatch) => {
+        
         fetch('http://127.0.0.1:3000/users')
         .then(resp => resp.json())
-        .then(users => dispatch({ type: 'FETCH_USERS', payload: users})) 
+        .then(users =>  {
+            dispatch({ type: 'FETCH_USERS', payload: users})
+        } )
     }
        
 } 
 
 export const addUser = user => {
-    console.log(user)
+    //console.log(user)
+    
     return dispatch => {
         fetch('http://127.0.0.1:3000/users.json', {
         method: 'POST', 
         body: JSON.stringify({user}),
-        headers: { 'Content-Type': 'application/json'}
+        headers: { 'Content-Type': 'application/json'} 
     })
     .then(resp => resp.json())
     .then(data => {
-       console.log(user) 
        dispatch({ type: 'ADD_USER', payload: data.user})
        
     }) 
 
-    }
+    } 
     
     
 
