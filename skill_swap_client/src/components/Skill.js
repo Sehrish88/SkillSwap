a6bbd42117248d7e4c047f78f39a5ee967645723
Sehrish88 <sehrishbalouch4@gmail.com>
2021-01-23 13:51:47 -0500

    add background color of sky blue to each skill

--- skill_swap_client/src/components/Skill.js
+++ skill_swap_client/src/components/Skill.js
@@ -1,7 +1,9 @@
 import React from 'react'
+import UpvoteButton from './UpvoteButton'
+import ToggleButton from './ToggleButton'
 
 const Skill = (props) =>{
-    return <ul><li key={props.skill.id} style= {{border: '3px solid green'}}><h4>Field: {props.skill.field}</h4>  <b>Years Experience: </b>{props.skill.years_experience}    </li></ul>
+    return <ul><li key={props.skill.id} style= {{border: '3px solid green', backgroundColor: 'skyblue'}}><h4>Field: {props.skill.field}</h4>  <b>Years Experience: </b>{props.skill.years_experience}  <UpvoteButton /> <ToggleButton /> </li></ul>
 }
 
 export default Skill
