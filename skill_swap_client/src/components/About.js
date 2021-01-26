0942c2e6f36218f5cdb925cad3f3151cc9b74d44
Sehrish88 <sehrishbalouch4@gmail.com>
2021-01-26 16:30:43 -0500

    adjust wiidth of border

--- skill_swap_client/src/components/About.js
+++ skill_swap_client/src/components/About.js
@@ -10,7 +10,7 @@ const About = () => {
     <div> 
     <Header /> 
     <HomeLink /> 
-    <h4 style={{color: 'green', textAlign: 'center', border: '5px solid green', backgroundColor: 'skyblue', justifyContent: 'center'}}> Skill swap is a platform created for the exchange of knowledge, ideas, and skills. It was designed to connect individuals with others who will help them grow and in turn, reduce the skills gap that we see in society today.   We aim for inclusivity and to make it easier for people to gain access to resources to which money may be a barrier in certain instances. Skill Swap’s main goal is to help people feel empowered, connected, and live life with a sense of purpose while helping societies become more functional.  </h4>
+    <h4 style={{color: 'green', textAlign: 'center', border: '5px solid green', backgroundColor: 'skyblue', justifyContent: 'center', width: '400'}}> Skill swap is a platform created for the exchange of knowledge, ideas, and skills. It was designed to connect individuals with others who will help them grow and in turn, reduce the skills gap that we see in society today.   We aim for inclusivity and to make it easier for people to gain access to resources to which money may be a barrier in certain instances. Skill Swap’s main goal is to help people feel empowered, connected, and live life with a sense of purpose while helping societies become more functional.  </h4>
 
     </div>
      )
