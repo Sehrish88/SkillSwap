7003eebe3a337d03b944631484bdfb9db81f3b8d
Sehrish88 <sehrishbalouch4@gmail.com>
2021-02-05 14:18:38 -0500

    add border radius to each rendered individual user

--- skill_swap_client/src/components/UsersList.js
+++ skill_swap_client/src/components/UsersList.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import HomeLink from './HomeLink'
+
 
 
 const UsersList = ({users}) => {
     
     return(
         <div>
-           <HomeLink />
-            {users.map(user => <ul><li key={user.id} style= {{border: '3px solid green', backgroundColor: 'skyblue'}}><h3>{user.name}</h3>  <b>Email: </b>{user.email} <br/>  <b>Field-</b> {user.skills[0].field} <br/> <b>Years of Experience-</b> {user.skills[0].years_experience} 
+        
+            {users.map(user => <ul><li key={user.id} style= {{border: '3px solid green', borderRadius: '25px', backgroundColor: 'skyblue', width: '350'}}><h3>{user.name}</h3>  <b>Email: </b>{user.email} <br/>  <b>Field-</b> {user.skills[0].field} <br/> <b>Years of Experience-</b> {user.skills[0].years_experience} 
             </li></ul>)}
             
             
