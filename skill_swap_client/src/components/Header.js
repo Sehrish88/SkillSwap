89e3167e9afa228e903ad66fa841b1353aefae43
Sehrish88 <sehrishbalouch4@gmail.com>
2021-02-05 14:10:46 -0500

    add border radius to header

--- skill_swap_client/src/components/Header.js
+++ skill_swap_client/src/components/Header.js
@@ -3,7 +3,7 @@ import {MdSwapCalls} from 'react-icons/md';
 
 const Header = () => {
 
-    return <h1 style={{color: 'green', textAlign: 'center', border: '5px solid green', backgroundColor: 'skyblue', className: 'header', width: '400'}}> Skill Swap <MdSwapCalls />  </h1>
+    return <h1 style={{color: 'green', textAlign: 'center', border: '5px solid green', backgroundColor: 'skyblue', className: 'header', width: '400', borderRadius: '25px' }}> Skill Swap <MdSwapCalls />  </h1>
 }
 
 export default Header; 
