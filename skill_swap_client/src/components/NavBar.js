ac6ecd68859e3f1146fa62795d3c4e277f8a6fad
Sehrish88 <sehrishbalouch4@gmail.com>
2021-02-05 14:13:24 -0500

    mae navbar horizontal

--- skill_swap_client/src/components/NavBar.js
+++ skill_swap_client/src/components/NavBar.js
@@ -3,14 +3,14 @@ import { Link, NavLink } from 'react-router-dom';
 
 const NavBar = () => {
     return (<nav className="nav">
-        <div className="Container">
+        <div className="navbar">
             <h6 style= {{color: 'green' }}><ul > 
-               <NavLink to="/">Home</NavLink> <span className="second-word-formatting"></span><br/>
-               <NavLink to="/about">About</NavLink> <span className="second-word-formatting"></span><br/>
-               <NavLink to="/register">Register</NavLink> <span className="second-word-formatting"></span><br/>
-               <NavLink to="/addSkill">Add Skill</NavLink> <span className="second-word-formatting"></span><br/>
-               <NavLink to="/users">Browse Users</NavLink> <span className="second-word-formatting"></span><br/>
-               <NavLink to="/skills">Browse skills</NavLink><br />
+               <NavLink to="/">Home</NavLink> <span className="second-word-formatting"></span>
+               <NavLink to="/about">About</NavLink> <span className="second-word-formatting"></span>
+               <NavLink to="/register">Register</NavLink> <span className="second-word-formatting"></span>
+               <NavLink to="/addSkill">Add Skill</NavLink> <span className="second-word-formatting"></span>
+               <NavLink to="/users">Browse Users</NavLink> <span className="second-word-formatting"></span>
+               <NavLink to="/skills">Browse skills</NavLink>
               
                
             </ul></h6>
