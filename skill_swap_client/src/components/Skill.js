d10ef8cabc5032201bcffc6eff4b6666c181d918
Sehrish88 <sehrishbalouch4@gmail.com>
2021-02-05 14:15:43 -0500

    add border radius to individual skills

--- skill_swap_client/src/components/Skill.js
+++ skill_swap_client/src/components/Skill.js
@@ -3,7 +3,7 @@ import UpvoteButton from './UpvoteButton'
 import ToggleButton from './ToggleButton'
 
 const Skill = (props) =>{
-    return <ul><li key={props.skill.id} style= {{border: '3px solid green', backgroundColor: 'skyblue'}}><h4>Field: {props.skill.field}</h4>  <b>Years Experience: </b>{props.skill.years_experience}  <UpvoteButton /> <ToggleButton /> </li></ul>
+    return <ul><li key={props.skill.id} style= {{border: '3px solid green', backgroundColor: 'skyblue', borderRadius: '25px'}}><h4>Field: {props.skill.field}</h4>  <b>Years Experience: </b>{props.skill.years_experience}  <UpvoteButton /> <ToggleButton /> </li></ul>
 }
 
 export default Skill
