11faea7bd10947478a300dd31e144aabe7b2f7a3
Sehrish88 <sehrishbalouch4@gmail.com>
2021-02-05 14:03:55 -0500

    no  change

--- skill_swap_api/config/routes.rb
+++ skill_swap_api/config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
+  get "login", to: "sessions#create"
   resources :sessions 
   resources :skills
   resources :registrations 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'pages#home'
   resources :sessions, only: [:create]
-  resources :users
+  resources :users 
   get 'logged_in', to: 'sessions#logged_in'
   resources :skills 
 end
