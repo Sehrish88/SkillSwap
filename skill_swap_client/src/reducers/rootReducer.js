9434c202386a65adfc0da727b913473afbec5b46
Sehrish88 <sehrishbalouch4@gmail.com>
2021-02-05 14:20:07 -0500

    add reducers to rootReducer

--- skill_swap_client/src/reducers/rootReducer.js
+++ skill_swap_client/src/reducers/rootReducer.js
@@ -2,11 +2,16 @@ import { combineReducers } from 'redux'
 import { usersReducer } from './usersReducer'
 import { skillsReducer } from './skillsReducer' 
 import authReducer from './authReducer'
+import currentUser from './currentUserReducer' 
+import currentUserReducer from './currentUserReducer'
+import loginFormReducer from './loginFormReducer'
 
  const rootReducer = combineReducers({
     users: usersReducer,
     skills: skillsReducer,
-    auth: authReducer 
+    auth: authReducer,  
+    currentUser: currentUserReducer,
+    loginFormReducer 
 }) 
 
 export default rootReducer 
