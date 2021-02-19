b4e9e5659a3ab532fa6defc52c402cab4af40609
Sehrish88 <sehrishbalouch4@gmail.com>
2021-02-19 13:59:51 -0500

    add styling for active links in navbar

--- skill_swap_client/src/components/NavBar.js
+++ skill_swap_client/src/components/NavBar.js
@@ -5,12 +5,12 @@ const NavBar = () => {
     return (<nav className="nav">
         <div className="navbar">
             <h6 style= {{color: 'green' }}><ul > 
-               <NavLink to="/">Home</NavLink> <span className="second-word-formatting"></span>
-               <NavLink to="/about">About</NavLink> <span className="second-word-formatting"></span>
-               <NavLink to="/register">Register</NavLink> <span className="second-word-formatting"></span>
-               <NavLink to="/addSkill">Add Skill</NavLink> <span className="second-word-formatting"></span>
-               <NavLink to="/users">Browse Users</NavLink> <span className="second-word-formatting"></span>
-               <NavLink to="/skills">Browse skills</NavLink>
+               <NavLink exact activeStyle={{color: 'green'}} to="/">Home</NavLink> <span className="second-word-formatting"></span>
+               <NavLink exact activeStyle={{color: 'green'}} to="/about">About</NavLink> <span className="second-word-formatting"></span>
+               <NavLink exact activeStyle={{color: 'green'}} to="/register">Register</NavLink> <span className="second-word-formatting"></span>
+               <NavLink exact activeStyle={{color: 'green'}} to="/addSkill">Add Skill</NavLink> <span className="second-word-formatting"></span>
+               <NavLink exact activeStyle={{color: 'green'}} to="/users">Browse Users</NavLink> <span className="second-word-formatting"></span>
+               <NavLink exact activeStyle={{color: 'green'}} to="/skills">Browse skills</NavLink>
               
                
             </ul></h6>
