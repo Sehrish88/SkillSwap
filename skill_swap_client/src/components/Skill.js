033c9f0a09d9c61b374ff691fd9c55b4ca1c350b
Sehrish88 <sehrishbalouch4@gmail.com>
2021-02-27 12:13:52 -0500

    remove Toggle Button

--- skill_swap_client/src/components/Skill.js
+++ skill_swap_client/src/components/Skill.js
@@ -1,9 +1,9 @@
 import React from 'react'
-import UpvoteButton from './UpvoteButton'
-import ToggleButton from './ToggleButton'
+import Liker from './Liker'
+
 
 const Skill = (props) =>{
-    return <ul><li key={props.skill.id} style= {{border: '3px solid green', backgroundColor: 'skyblue', borderRadius: '25px'}}><h4>Field: {props.skill.field}</h4>  <b>Years Experience: </b>{props.skill.years_experience}  <UpvoteButton /> <ToggleButton /> </li></ul>
+    return <ul><li key={props.skill.id} style= {{border: '3px solid green', backgroundColor: 'skyblue', borderRadius: '25px'}}><h4>Field: {props.skill.field}</h4>  <b>Years Experience: </b>{props.skill.years_experience} {<Liker />}  </li></ul>
 }
 
 export default Skill
